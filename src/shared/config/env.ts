3f3238e060ab5361e74874ae52337d86a3d37ff1
purrweb <purrweb@MacBook-Pro-purrweb.local>
2026-05-04 11:10:54 +0600

    fix: update client ids, package name and bundle identifier

--- src/shared/config/env.ts
+++ src/shared/config/env.ts
@@ -19,11 +19,11 @@ export const ENV: EnvShape = {
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind0dm9xY3B4cXNnbXFmaW13cGJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1MjUxODcsImV4cCI6MjA5MzEwMTE4N30.3JLEVv3qWbOli3dm6iwb2LMMA36A4zVPXxwyfSymW_o",
   WEB_CLIENT_ID:
     webClientId ||
-    "166459366038-cra1coha58ggg2ht3dtpcdnc049ssulr.apps.googleusercontent.com",
+    "166459366038-fbetjvo5rpv7gf255kpi1rmp1g28klkq.apps.googleusercontent.com",
   ANDROID_CLIENT_ID:
     androidClientId ||
-    "166459366038-u4upnv9u0bl2kilnud6skeeskoqm9va5.apps.googleusercontent.com",
+    "166459366038-67kvgp40ru1r3d60hf2hhh2uupvo2bpl.apps.googleusercontent.com",
   IOS_CLIENT_ID:
     iosClientId ||
-    "166459366038-vtttpqu1uemivlv0e9ppccqrtj5uo8p2.apps.googleusercontent.com",
+    "166459366038-mqigb285j43lvm4c4v42ktiptj50702m.apps.googleusercontent.com",
 };
